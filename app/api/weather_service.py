d14e9b747f6ed6aaed69cd9a0fafb46fc17c00f2
K3nse1 <raulsangut999@gmail.com>
2025-12-06 09:46:11 +0100

    Endpoints refinados

--- app/api/weather_service.py
+++ app/api/weather_service.py
@@ -20,22 +20,20 @@ def post_city(city_data: CityCreate) -> Weather:
     cities.append(new_city)
     return Weather(**new_city)
 
-def put_city(city:str, temperature:int, description:str):
-    if temperature < -40 or temperature > 50:
-        raise ValueError("The temperature provided makes no sense")
+def put_city(city_data:CityCreate) -> Weather:
     
-    city = city.strip().lower()
+    city = city_data.city.strip().lower()
     for place in cities:
         if place["city"].lower() == city:
-            place["temperature"] = f"{temperature}ºC"
-            place["description"] = description
-            return {"detail": f"{place["city"]} has been updated successfully."}
+            place["temperature"] = f"{city_data.temperature}ºC"
+            place["description"] = city_data.description
+            return Weather(**place)
     raise KeyError("The city provided does not exist")
 
-def delete_city(city:str):
-    city = city.strip().lower()
+def delete_city(city_name:CityDelete) -> MessageResponse:
+    city = city_name.city.strip().lower()
     for idx, place in enumerate(cities):
         if place["city"].lower() == city:
             cities.pop(idx)
-            return {"detail": f'{place["city"]} removed successfully.'}
+            return MessageResponse(detail=f"{city_name.city} has been removed successfully.")
     raise KeyError ("The city provided does not exist.")
